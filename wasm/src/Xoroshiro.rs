13b4c435b7e7c05dd2f30b17ddf1c75c7727e578
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2021-11-18 12:11:08 -0600

    Refactor

---
+++ wasm/src/Xoroshiro.rs
@@ -0,0 +1,77 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Xoroshiro {
+    s0: u64,
+    s1: u64,
+}
+
+impl Xoroshiro {
+    pub fn new(seed: u64) -> Self {
+        Self {
+            s0: seed,
+            s1: 0x82A2B175229D6A5B,
+        }
+    }
+
+    pub fn from_state(s0: u64, s1: u64) -> Self {
+        Self { s0, s1 }
+    }
+
+    pub fn next_u64(&mut self) -> u64 {
+        let result = self.s0.wrapping_add(self.s1);
+
+        self.s1 ^= self.s0;
+        self.s0 = self.s0.rotate_left(24) ^ self.s1 ^ (self.s1 << 16);
+        self.s1 = self.s1.rotate_left(37);
+
+        result
+    }
+
+    pub fn next(&mut self) -> u32 {
+        self.next_u64() as u32
+    }
+
+    pub fn get_state(&self) -> (u64, u64) {
+        (self.s0, self.s1)
+    }
+
+    pub fn advance_to_state(&mut self, state: (u64, u64)) -> Option<u64> {
+        let mut advances = 0;
+
+        // 10,000 is an arbitary limit to avoid an infinite loop
+        while self.get_state() != state {
+            self.next();
+            advances += 1;
+
+            if advances > 10_000 {
+                return None;
+            }
+        }
+
+        Some(advances)
+    }
+
+    fn get_mask(num: u32) -> u32 {
+        let mut result = num - 1;
+
+        for i in 0..5 {
+            let shift = 1 << i;
+            result |= result >> shift;
+        }
+
+        result
+    }
+
+    pub fn rand_max(&mut self, max: u32) -> u32 {
+        let mask = Self::get_mask(max);
+        let mut rand = self.next() & mask;
+
+        while max <= rand {
+            rand = self.next() & mask;
+        }
+
+        rand
+    }
+}
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,81 +3,8 @@ use num_enum::FromPrimitive;
 use std::convert::TryFrom;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub struct Xoroshiro {
-    s0: u64,
-    s1: u64,
-}
-
-impl Xoroshiro {
-    pub fn new(seed: u64) -> Self {
-        Self {
-            s0: seed,
-            s1: 0x82A2B175229D6A5B,
-        }
-    }
-
-    pub fn from_state(s0: u64, s1: u64) -> Self {
-        Self { s0, s1 }
-    }
-
-    pub fn next_u64(&mut self) -> u64 {
-        let result = self.s0.wrapping_add(self.s1);
-
-        self.s1 ^= self.s0;
-        self.s0 = self.s0.rotate_left(24) ^ self.s1 ^ (self.s1 << 16);
-        self.s1 = self.s1.rotate_left(37);
-
-        result
-    }
-
-    pub fn next(&mut self) -> u32 {
-        self.next_u64() as u32
-    }
-
-    pub fn get_state(&self) -> (u64, u64) {
-        (self.s0, self.s1)
-    }
-
-    pub fn advance_to_state(&mut self, state: (u64, u64)) -> Option<u64> {
-        let mut advances = 0;
-
-        // 10,000 is an arbitary limit to avoid an infinite loop
-        while self.get_state() != state {
-            self.next();
-            advances += 1;
-
-            if advances > 10_000 {
-                return None;
-            }
-        }
-
-        Some(advances)
-    }
-
-    fn get_mask(num: u32) -> u32 {
-        let mut result = num - 1;
-
-        for i in 0..5 {
-            let shift = 1 << i;
-            result |= result >> shift;
-        }
-
-        result
-    }
-
-    pub fn rand_max(&mut self, max: u32) -> u32 {
-        let mask = Self::get_mask(max);
-        let mut rand = self.next() & mask;
-
-        while max <= rand {
-            rand = self.next() & mask;
-        }
-
-        rand
-    }
-}
+mod xoroshiro;
+pub use xoroshiro::*;
 
 #[wasm_bindgen]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -253,7 +180,8 @@ fn generate_static_pokemon(mut rng: Xoroshiro, tid: u16, sid: u16, shiny_charm: 
     }
 }
 
-struct Pokemon {
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct Pokemon {
     shiny_type: ShinyEnum,
     ec: u32,
     pid: u32,
@@ -342,6 +270,18 @@ pub struct ShinyResult {
     pub ability: u32,
 }
 
+pub fn filter(
+    results: Pokemon,
+    shiny_filter: ShinyFilterEnum,
+    nature_filter: NatureFilterEnum,
+) -> bool {
+    if shiny_filter == results.shiny_type && nature_filter == results.nature {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 #[wasm_bindgen]
 pub fn calculate_pokemon(
     seed1: u64,
@@ -369,7 +309,7 @@ pub fn calculate_pokemon(
             }
         };
 
-        if shiny_filter == pokemon_results.shiny_type && nature_filter == pokemon_results.nature {
+        if filter(pokemon_results, shiny_filter, nature_filter) {
             let shiny_state = rng.get_state();
             let result = ShinyResult {
                 state0: shiny_state.0,
