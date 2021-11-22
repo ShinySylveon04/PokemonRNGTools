3d3ca0b78f3c937c4e3048fcb9374c50530523a5
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2021-11-22 16:05:14 -0600

    Fix routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { SwSh } from './SwSh/SwSh';
 import { BdSp } from './BdSp/BdSp';
 
 const app = document.getElementById('app');
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<SwSh />} />
       <Route path="bdsp" element={<BdSp />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   app,
 );
